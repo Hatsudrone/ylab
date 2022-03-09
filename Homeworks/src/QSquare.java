5e65c5fbdc8a3326010f40080a3dc00df5a70add
Mukhamed Khalbaev <angelofside@gmail.com>
2022-03-09 14:19:52 +0300

    Homeworks 2 lection

--- Homeworks/src/QSquare.java
+++ Homeworks/src/QSquare.java
@@ -1,10 +1,16 @@
 public class QSquare {
     public static void main(String[] args) {
-        int i = 1, x = 1;
-       for (i = 0; i <9; i++){
-           System.out.println("S");
-           for (x = 0; x <9; x++)
-               System.out.print("S");
+        int num = 0;   //напишите тут ваш код
+        while ( num <5 ){
+            int m = 0;
+            while (m <10){
+                System.out.print("S");
+                m++;
+            }
+            System.out.println();
+            num++;
         }
+
+
     }
 }
